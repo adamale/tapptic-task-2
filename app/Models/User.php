eb1dd1079bfc668fc5a7028ecb344bab2996962e
Adam Aleksak <adamale91@gmail.com>
2022-07-20 19:35:30 +0200

    Created likes relationship

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,7 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
+/**
+ * @property int $id
+ * @property string $name
+ * @property \Illuminate\Support\Collection<\App\Models\User> $likedUsers
+ * @property \Illuminate\Support\Collection<\App\Models\User> $likedBy
+ */
 class User extends Model
 {
     use HasFactory;
@@ -14,4 +21,14 @@ class User extends Model
     ];
 
     public $timestamps = false;
+
+    public function likedUsers(): BelongsToMany
+    {
+        return $this->belongsToMany(User::class, 'likes', 'user_a_id', 'user_b_id');
+    }
+
+    public function likedBy(): BelongsToMany
+    {
+        return $this->belongsToMany(User::class, 'likes', 'user_b_id', 'user_a_id');
+    }
 }
